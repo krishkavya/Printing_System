7bcebd4f4ded12b41eaca341a05557fb85e7c071
Poonam C Sanil <83509031+1262PCS@users.noreply.github.com>
2021-12-23 20:34:34 +0530

    Update login_connection.java

--- Code/login_connection.java
+++ Code/login_connection.java
@@ -1,22 +1,27 @@
 package sample1;
 import java.sql.*;  
 
-public class login_connection 
+public class login_connection extends login_function
 {public static void main(String args[]){
 	try
 {  
 	  Class.forName("org.postgresql.Driver");  
 	  Connection connect=DriverManager.getConnection("jdbc:postgresql://localhost:5432/Printing System","postgres","pcssql1234");  
 	  Statement stmt=connect.createStatement();  
-	  ResultSet rs=stmt.executeQuery("select * from Faculty_details");  
-	  while(rs.next())  
+	  ResultSet f_id=stmt.executeQuery("select F_ID from Faculty_details");  
+	  while(f_id.next())  
 		     
-	  { if(rs==tf1)
+	  { if(f_id==tf1)
 		    {break;}
 	  }
-		    
+         ResultSet c_id=stmt.executeQuery("select C_ID from Clerk");  
+	  while(c_id.next())  
+		     
+	  { if(c_id==tf1)
+		    {break;}
+	  }    
 		   
-	  System.out.println(rs.getString(2));  
+	 
 	  connect.close();  
 	}
 	catch(Exception e)
